2f256d7b7ff541caca591ca1e8ad821df4907d17
SimonVandesompele <115065783+SimonVandesompele@users.noreply.github.com>
2023-10-09 11:23:42 +0200

    Initial commit

---
+++ PeopleManager.Cyb.Ui.Mvc/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PeopleManager.Cyb.Ui.Mvc.Models;
+using System.Diagnostics;
+using PeopleManager.Cyb.Ui.Mvc.Core;
+
+namespace PeopleManager.Cyb.Ui.Mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly Database _database;
+
+        public HomeController(Database database)
+        {
+            _database = database;
+        }
+
+        public IActionResult Index()
+        {
+            var people = _database.People;
+            return View(people);
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+    }
+}
---
+++ PeopleManager.Cyb.Ui.Mvc/Core/Database.cs
@@ -0,0 +1,25 @@
+﻿using PeopleManager.Cyb.Ui.Mvc.Models;
+
+namespace PeopleManager.Cyb.Ui.Mvc.Core
+{
+    public class Database
+    {
+        public IList<Person> People { get; set; } = new List<Person>();
+
+        public void Seed()
+        {
+            People = new List<Person>
+            {
+                new Person
+                {
+                    FirstName = "Bavo",
+                    LastName = "Ketels",
+                    Email = "bavo.ketels@gmail.com"
+                },
+                new Person{FirstName = "Bavo2", LastName = "Ketels2", Email = "bavo.ketels2@gmail.com"},
+                new Person{FirstName = "Bavo3", LastName = "Ketels3", Email = "bavo.ketels3@gmail.com"},
+                new Person{FirstName = "Bavo4", LastName = "Ketels4", Email = "bavo.ketels4@gmail.com"}
+            };
+        }
+    }
+}
